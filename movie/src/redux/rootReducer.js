e06e74fb493e642a3c9606276b842c0bc667a346
phonglvh <phonglvh996@gmail.com>
2021-07-26 10:46:17 +0700

    'apply_store_middleware'

---
+++ movie/src/redux/rootReducer.js
@@ -0,0 +1,14 @@
+import { combineReducers, compose } from "redux";
+import thunk from "redux-thunk";
+
+const rootReducer = combineReducers({
+
+})
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+	rootReducer,
+	composeEnhancers(applyMiddleware(thunk))
+)
+
+export default store
